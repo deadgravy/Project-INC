de1a5d0a22a264f548633c457e6bc9bd65e6b2f0
Shelby <yeoxhshelby20.21@ichat.sp.edu.sg>
2022-07-02 15:31:05 +0800

    Added files for sign up backend

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,36 @@
+const userService = require('../services/userService');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+module.exports.addUser = async function (req, res, next) {
+
+    try {
+        const {email, contact_number, first_name, last_name, password} = req.body;
+        console.log(0);
+
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            bcrypt.hash(password, salt, function(err, hash) {
+            // returns hash
+            console.log(hash);
+            
+            let data = {email, contact_number, first_name, last_name, hash}
+            userService.addUser(email, contact_number, first_name, last_name, hash);
+            console.log(data)
+            });
+          });
+
+          // response for duplicate entry not done
+
+
+        res.status(201).json({
+            status: 'User added successfully!',
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+        status: 'fail',
+        data: null
+        });
+    }
+
+} // End of async function(req,res,next)
--- backend/src/routes/dataRoutes.js
+++ backend/src/routes/dataRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+router.use(express.json())
 const dataController = require('../controllers/dataController');
+const userController = require('../controllers/userController')
 
 router.get('/data/data1', dataController.getData1);
 
@@ -10,5 +12,8 @@ router.get('/data/data2/:id', dataController.getRecipebyRecipeID);
 
 // login
 // router.get('/login', dataController.verify)
+
+// add user
+router.post('/adduser', userController.addUser)
 
 module.exports = router;
