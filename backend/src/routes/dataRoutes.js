3961e07d66b74dc3245a1d44b89323943c2b4d33
Shelby <yeoxhshelby20.21@ichat.sp.edu.sg>
2022-07-02 15:31:05 +0800

    Added files for sign up backend

--- backend/src/routes/dataRoutes.js
+++ backend/src/routes/dataRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+router.use(express.json())
 const dataController = require('../controllers/dataController');
+const userController = require('../controllers/userController')
 
 router.get('/data/data1', dataController.getData1);
 
@@ -9,5 +11,8 @@ router.get('/data/data2', dataController.getRecipebyRecipeID);
 
 // login
 // router.get('/login', dataController.verify)
+
+// add user
+router.post('/adduser', userController.addUser)
 
 module.exports = router;
