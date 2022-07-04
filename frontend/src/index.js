471b36a08ecc7120df0daac4ee7bc568f1332e61
georgeyinxu <georgeyinxu@gmail.com>
2022-07-04 22:09:23 +0800

    Created sidebar UI with links to navigate through web app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "./App";
-import ProductionOverview from "./pages/ProductionOverview";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './App';
+import ProductionOverview from './pages/ProductionOverview';
+import Sidebar from './components/sidebar/Sidebar';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const sidebar = ReactDOM.createRoot(document.getElementById('sidebar'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="productionOverview" element={<ProductionOverview />} />
+      <Route path='/' element={<App />} />
+      <Route path='productionOverview' element={<ProductionOverview />} />
     </Routes>
   </BrowserRouter>
 );
-//hello world
+
+sidebar.render(
+  <React.StrictMode>
+    <Sidebar />
+  </React.StrictMode>
+)
