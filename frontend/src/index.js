28ab596f47d196f23a1bd03d223904eb015676d8
mememlol <88182648+mememlol@users.noreply.github.com>
2022-07-05 16:33:28 +0800

    Fixed rebase conflict 1/3

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import ProductionOverview from "./pages/ProductionOverview";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+<<<<<<< HEAD
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
@@ -15,4 +16,12 @@ root.render(
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
+=======
+  <BrowserRouter>
+    <Routes>
+      <Route path="login" element={<App />} />
+      <Route path="productionOverview" element={<ProductionOverview />} />
+    </Routes>
+  </BrowserRouter>
+>>>>>>> 7ae010e4 (changed path to login in index.js line 11)
 );
