ce7664cb2965da4cd1f307c50c95e23c4b03f973
mememlol <88182648+mememlol@users.noreply.github.com>
2022-07-05 16:33:28 +0800

    Fixed rebase conflict 1/3

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,7 @@ import moment from 'moment';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+<<<<<<< HEAD
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
@@ -21,4 +22,12 @@ root.render(
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
+=======
+  <BrowserRouter>
+    <Routes>
+      <Route path="login" element={<App />} />
+      <Route path="productionOverview" element={<ProductionOverview />} />
+    </Routes>
+  </BrowserRouter>
+>>>>>>> 7ae010e4 (changed path to login in index.js line 11)
 );
