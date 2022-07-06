3866ad65d097248ca7419bab077610512157965e
Javier <JAVIERFOKJF11.21@ichat.sp.edu.sg>
2022-07-06 16:37:03 +0800

    Adding of today's production chart

--- backend/src/services/dataService.js
+++ backend/src/services/dataService.js
@@ -6,8 +6,8 @@ module.exports.getData1 = async function () {
     host: "localhost",
     user: "postgres",
     port: 5432,
-    password: "gyx915826",
-    database: "FIRC",
+    password: "postgres",
+    database: "FIRC_v3",
   });
 
   client.connect();
@@ -60,8 +60,8 @@ module.exports.getRecipebyRecipeID = async function (id) {
     host: "localhost",
     user: "postgres",
     port: 5432,
-    password: "gyx915826",
-    database: "FIRC",
+    password: "postgres",
+    database: "FIRC_v3",
   });
 
   client.connect();
@@ -108,5 +108,83 @@ module.exports.getRecipebyRecipeID = async function (id) {
     return rows;
   } catch (error) {
     console.log(error);
+  }
+};
+
+module.exports.getCompletedProducts = async function () {
+
+
+  const client = new Client({
+      host: "localhost",
+      user: "postgres",
+      port: 5432,
+      password: "postgres",
+      database: "FIRC_v3"
+  })
+
+  client.connect();
+  try {
+
+
+      const { rows } = await client.query(`SELECT lt.recipe_id, rp.name, COUNT(*) AS batchesCompleted
+      FROM log_times lt
+      INNER JOIN recipes rp
+      ON lt.recipe_id = rp.id
+      INNER JOIN recipe_flows AS rf
+      ON lt.recipe_id = rf.fr_recipe_id
+      WHERE lt.log_time BETWEEN '2021-08-21 00:00:00' AND '2021-08-21 08:00:00'
+      AND rf.queue = 1
+      AND lt.log_action = 2
+      GROUP BY lt.recipe_id, rf.id, rp.name, rf.queue, rf.id
+      ORDER BY lt.recipe_id;
+      `);
+
+      client.end();
+
+      return rows;
+
+  } catch (error) {
+      console.log(error);
+  } finally {
+      client.end();
+  }
+};
+
+module.exports.getProductsToComplete = async function () {
+
+
+  const client = new Client({
+      host: "localhost",
+      user: "postgres",
+      port: 5432,
+      password: "postgres",
+      database: "FIRC_v3"
+  })
+
+  client.connect();
+  try {
+
+
+      const { rows } = await client.query(`SELECT lt.recipe_id, rp.name, COUNT(*) AS batchesToComplete
+      FROM log_times lt
+      INNER JOIN recipes rp
+      ON lt.recipe_id = rp.id
+      INNER JOIN recipe_flows AS rf
+      ON lt.recipe_id = rf.fr_recipe_id
+      WHERE lt.log_time BETWEEN '2021-08-21 00:00:00' AND '2021-08-21 23:59:59'
+      AND rf.queue = 1
+      AND lt.log_action = 2
+      GROUP BY lt.recipe_id, rf.id, rp.name, rf.queue, rf.id
+      ORDER BY lt.recipe_id;
+      `);
+
+      client.end();
+
+      return rows;
+
+  } catch (error) {
+      console.log(error);
+  } finally {
+      client.end();
   }
 };
---
+++ frontend/src/pages/TodayProduction.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import "../styles/styles.css";
+import ProductChart from "../components/tpd/ProductChart";
+// import "../styles/todayProduction.css"
+
+const TodayProduction = () => {
+  const [completedProductsData, setCompletedProductsData] = useState(null);
+  const [toBeCompletedProductsData, setToBeCompletedProductsData] = useState(null);
+  const [isLoading, setIsloading] = useState(true);
+
+  // useEffect
+  useEffect(() => {
+
+    setIsloading(true);
+
+    Promise.all([
+      fetch("http://localhost:4000/api/getCompletedProducts").then((res) => res.json()),
+      fetch("http://localhost:4000/api/getProductsToComplete").then((res) => res.json()),
+    ])
+      .then(([result1, result2]) => {
+        setCompletedProductsData({
+          data: result1.data,
+          value: [{ value: 50 }, { value: 50 }],
+        });
+        setToBeCompletedProductsData({
+          data: result2.data
+        });
+        setIsloading(false);
+      })
+      .catch((error) => console.log("error", error));
+
+   
+  }, []);
+
+  return (
+    <div className="App">
+      {!isLoading ? (
+        
+        <div className="Row1">
+          <ProductChart data1={completedProductsData} data2={toBeCompletedProductsData} />
+          </div>
+      ) : (
+        <div> Loading... </div> // TODO: Loading loading spinner or loading component
+      )}
+    </div>
+  );
+};
+
+export default TodayProduction;
