31b7a60031d7c74191d41c2d4b1c2cfb207e2e3d
Adeeb <adityaadeeb04@gmail.com>
2022-07-08 18:04:17 +0800

    adding and testing storybook.js

--- frontend/src/pages/ProductionOverview.js
+++ frontend/src/pages/ProductionOverview.js
@@ -77,27 +77,7 @@ const ProductionOverview = () => {
       });
   }, []);
   const [modal, setModal] = useState(false);
-  // class ErrorBoundary extends React.Component {
-  //   constructor(props) {
-  //     super(props);
-  //     this.state = { hasError: false };
-  //   }
 
-  //   componentDidCatch(error, info) {
-  //     // Display fallback UI
-  //     this.setState({ hasError: true });
-  //     // You can also log the error to an error reporting service
-  //     console.log(error, info);
-  //   }
-
-  //   render() {
-  //     if (this.state.hasError) {
-  //       // You can render any custom fallback UI
-  //       return <h1>No recipes found.</h1>;
-  //     }
-  //     return this.props.children;
-  //   }
-  // }
   return (
     <React.StrictMode>
       <div className='App'>
@@ -118,9 +98,8 @@ const ProductionOverview = () => {
           </div>
         ) : (
           <div>
-            {' '}
-            <Loading />{' '}
-          </div> // TODO: Loading loading spinner or loading component
+            <Loading />
+          </div> // Loading
         )}
       </div>
     </React.StrictMode>
