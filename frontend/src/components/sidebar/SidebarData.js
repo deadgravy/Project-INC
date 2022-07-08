37e3de843a78050405700a9e7628cce5486111ca
Adeeb <adityaadeeb04@gmail.com>
2022-07-08 18:06:04 +0800

    fixing routes to production Overview Dashboard

--- frontend/src/components/sidebar/SidebarData.js
+++ frontend/src/components/sidebar/SidebarData.js
@@ -1,46 +1,46 @@
-import React from "react";
+import React from 'react';
 import HomeIcon from '@mui/icons-material/Home';
 import PersonIcon from '@mui/icons-material/Person';
-import SettingsIcon from '@mui/icons-material/Settings';   
-import WaterfallChartIcon from '@mui/icons-material/WaterfallChart'; 
+import SettingsIcon from '@mui/icons-material/Settings';
+import WaterfallChartIcon from '@mui/icons-material/WaterfallChart';
 import TodayIcon from '@mui/icons-material/Today';
-import DonutLarge from '@mui/icons-material/DonutLarge'
-import InventoryIcon from '@mui/icons-material/Inventory'
+import DonutLarge from '@mui/icons-material/DonutLarge';
+import InventoryIcon from '@mui/icons-material/Inventory';
 
 export const SidebarData = [
-    {
-        title: "Home",
-        icon: <HomeIcon />,
-        link: "/equipmentUtilisationSnapshot"
-    },
-    {
-        title: "Equipment Utilisation",
-        icon: <WaterfallChartIcon />,
-        link: "/equipmentUtilisationDashboard"
-    },
-    {
-        title: "Today's Production",
-        icon: <TodayIcon />,
-        link: "/todayProduction"
-    },
-    {
-        title: "Single Product Flow",
-        icon: <InventoryIcon />,
-        link: "/singleProduct"
-    },
-    {
-        title: "Production Overview",
-        icon: <DonutLarge />,
-        link: "/productionOverview"
-    },
-    {
-        title: "Settings",
-        icon: <SettingsIcon />,
-        link: "/settings"
-    },
-    {
-        title: "Profile",
-        icon: <PersonIcon />,
-        link: "/profile"
-    },
-]
+  {
+    title: 'Home',
+    icon: <HomeIcon />,
+    link: '/equipmentUtilisationSnapshot',
+  },
+  {
+    title: 'Equipment Utilisation',
+    icon: <WaterfallChartIcon />,
+    link: '/equipmentUtilisationDashboard',
+  },
+  {
+    title: "Today's Production",
+    icon: <TodayIcon />,
+    link: '/todayProduction',
+  },
+  {
+    title: 'Single Product Flow',
+    icon: <InventoryIcon />,
+    link: '/singleProduct',
+  },
+  {
+    title: 'Production Overview',
+    icon: <DonutLarge />,
+    link: '/productionOverview/63',
+  },
+  {
+    title: 'Settings',
+    icon: <SettingsIcon />,
+    link: '/settings',
+  },
+  {
+    title: 'Profile',
+    icon: <PersonIcon />,
+    link: '/profile',
+  },
+];
