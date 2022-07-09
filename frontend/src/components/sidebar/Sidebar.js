35da878d4fb49e29cb9f53c33fdd18e80cc5ba1e
George Yin Xu <georgeyinxu@gmail.com>
2022-07-09 22:40:57 +0800

    Fixed rebase conflicts from main

--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -4,25 +4,20 @@ import { SidebarData } from './SidebarData';
 
 const Sidebar = () => {
   return (
-    <div className='card h-screen p-2'>
-      <img src={require('../../logos/firc-logo.png')} alt='' />
-      <ul>
-        {SidebarData.map((val, key) => {
-          return (
-            <li
-              key={key}
-              className='row level px-1'
-              id={window.location.pathname == val.link ? 'active' : ''}
-              onClick={() => {
-                window.location.pathname = val.link;
-              }}
-            >
-              <div className='mt-1'>{val.icon}</div>
-              <div className='u-text-left'>{val.title}</div>
-            </li>
-          );
-        })}
-      </ul>
+    <div className="card h-screen p-2">
+        <img src={require('../../logos/firc-logo.png')} alt=''/>
+        <ul className="p-0 m-0">
+            {SidebarData.map((val, key) => {
+                return (
+                    <li key={key} className="row level px-1" style={{justifyContent: 'left'}} id={window.location.pathname == val.link ? "active" : ""} onClick={() => {
+                        window.location.pathname = val.link;
+                    }}>
+                        <div className="mt-1">{val.icon}</div>
+                        <div className="mx-2">{val.title}</div>
+                    </li>
+                )
+            })}
+        </ul>
     </div>
   );
 };
