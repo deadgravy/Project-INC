da29480574f3e0021a83bce3669d5fd4516b5ba1
Javier <JAVIERFOKJF11.21@ichat.sp.edu.sg>
2022-07-09 22:23:01 +0800

    adding table component and editing some styles

---
+++ frontend/src/components/tpd/TableComponent.js
@@ -0,0 +1,67 @@
+import React from 'react'
+
+const TableComponent = () => {
+  return (
+    <div className='table'>
+    <div class="p-4 bg-white-500 u-shadow-lg u-round-xs">
+    <table class="table">
+    <thead>
+        <tr>
+            <th>Equipment:</th>
+            <th>Current Recipe:</th>
+            <th>Equipment running for:</th>
+            <th>Stage:</th>
+            <th>Average time taken:</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Row:1 Cell:1</td>
+            <td>Row:1 Cell:2</td>
+            <td>Row:1 Cell:3</td>
+            <td>Row:1 Cell:4</td>
+            <td>S</td>
+        </tr>
+        <tr>
+            <td>Row:2 Cell:1</td>
+            <td>Row:2 Cell:2</td>
+            <td>Row:2 Cell:3</td>
+            <td>Row:2 Cell:4</td>
+            <td>S</td>
+        </tr>
+        <tr>
+            <td>Row:3 Cell:1</td>
+            <td>Row:3 Cell:2</td>
+            <td>Row:3 Cell:3</td>
+            <td>Row:3 Cell:4</td>
+            <td>S</td>
+        </tr>
+        <tr>
+            <td>Row:4 Cell:1</td>
+            <td>Row:4 Cell:2</td>
+            <td>Row:4 Cell:3</td>
+            <td>Row:4 Cell:4</td>
+            <td>S</td>
+        </tr>
+        <tr>
+            <td>Row:5 Cell:1</td>
+            <td>Row:5 Cell:2</td>
+            <td>Row:5 Cell:3</td>
+            <td>Row:5 Cell:4</td>
+            <td>S</td>
+        </tr>
+        <tr>
+            <td>Row:6 Cell:1</td>
+            <td>Row:6 Cell:2</td>
+            <td>Row:6 Cell:3</td>
+            <td>Row:6 Cell:4</td>
+            <td>S</td>
+        </tr>
+    </tbody>
+</table>
+</div>
+</div>
+  )
+}
+
+export default TableComponent;
--- frontend/src/pages/TodayProduction.js
+++ frontend/src/pages/TodayProduction.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "../styles/styles.css";
 import ProductChart from "../components/tpd/ProductChart";
+import TableComponent from "../components/tpd/TableComponent";
+import TextField from "@mui/material/TextField";
+import HorizontalScroller from 'react-horizontal-scroll-container';
+import { Tab } from "@mui/material";
 // import "../styles/todayProduction.css"
-// testing
+
 const TodayProduction = () => {
   const [completedProductsData, setCompletedProductsData] = useState(null);
   const [toBeCompletedProductsData, setToBeCompletedProductsData] = useState(null);
@@ -36,9 +40,44 @@ const TodayProduction = () => {
     <div className="App">
       {!isLoading ? (
         
-        <div className="Row1">
-          <ProductChart data1={completedProductsData} data2={toBeCompletedProductsData} />
+        <div>
+          <div className="tpdHeader">
+            Today's Production Dashboard
           </div>
+          <div className="liveEquipHeader">
+            Live Equipment Usage
+          </div>
+          <div className="textField">
+                <TextField 
+                id="outlined-basic"
+                variant="outlined"
+                fullWidth
+                label="Search"
+                />
+          </div>
+          <div className="Row1">
+              <TableComponent />
+          </div>
+          <div className="productHeader">
+            Completed Products as of Today
+          </div>
+          <div className="textField">
+                <TextField 
+                id="outlined-basic"
+                variant="outlined"
+                fullWidth
+                label="Search"
+                />
+          </div>
+          <div className="Row2">
+            <div className="chartContainer">
+              <div class="p-3 bg-white-500 u-shadow-lg u-round-xs">
+                <ProductChart data1={completedProductsData} data2={toBeCompletedProductsData} />
+              </div>
+            </div>
+          </div>
+        </div>
+        
       ) : (
         <div> Loading... </div> // TODO: Loading loading spinner or loading component
       )}
