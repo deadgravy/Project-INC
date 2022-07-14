b83a7a166e1bbc428a55ab9098af42381f121891
shelby <92902725+yeoxhshelby@users.noreply.github.com>
2022-07-14 22:01:56 +0800

    Equipment Utilisation Dashboard (#40)
    * Added frontend and backend for bar charts
    
    * Added datepicker UI and linked it to backend code
    
    * Single product flow (#41)
    
    * changed path to login in index.js line 11
    
    * Added SPFD.html and sidebar.css at frontend under pages
    
    * Added SingleProductFlow.js and spdf.css
    
    * Fixed rebase conflict 1/3
    
    * Added SPFD.html and sidebar.css at frontend under pages
    
    * Added SingleProductFlow.js and spdf.css
    
    * Fixed rebase conflict 1/3
    
    * Added SPFD.html and sidebar.css at frontend under pages
    
    * Added SingleProductFlow.js and spdf.css
    
    * Added SPFD.html and sidebar.css at frontend under pages
    
    * Added SingleProductFlow.js and spdf.css
    
    * Implemented UI for date and added backend for gantt chart
    
    * Implemented UI for date and added backend for gantt chart
    
    * Fixed rebase conflict 1/3
    
    * Added SPFD.html and sidebar.css at frontend under pages
    
    * Added SingleProductFlow.js and spdf.css
    
    * Added SPFD.html and sidebar.css at frontend under pages
    
    * Added SingleProductFlow.js and spdf.css
    
    * fix merge confilcs
    
    * Implemented UI for date and added backend for gantt chart
    
    * Fixed rebase conflict 1/3
    
    * Added SPFD.html and sidebar.css at frontend under pages
    
    * fix merge conflics
    
    * Added SPFD.html and sidebar.css at frontend under pages
    
    * fix merge conflics
    
    * Fixed merge conflicts
    
    * Fixed merge conflicts
    
    Co-authored-by: Louis <88182648+mememlol@users.noreply.github.com>

--- backend/src/routes/dataRoutes.js
+++ backend/src/routes/dataRoutes.js
@@ -6,6 +6,7 @@ const dataController = require('../controllers/dataController');
 const loginController = require('../controllers/loginController');
 const userController = require('../controllers/userController');
 const spfdController = require('../controllers/spfdController');
+const eudController = require('../controllers/eudController');
 
 router.get('/data/data1', dataController.getData1);
 
@@ -25,6 +26,12 @@ router.get('/getCompletedProducts', dataController.getCompletedProducts);
 //getting products to complete
 router.get('/getProductsToComplete', dataController.getProductsToComplete);
 
+//getting single recipe equipment usage
+router.get('/getSingleUsage/:date', eudController.getSingleUsage);
+
+// getting multiple recipe equipment usage
+router.get('/getMultipleUsage/:date', eudController.getMultipleUsage);
+
 //getting equipment status
 router.get('/getEquipmentStatus', dataController.getEquipmentStatus);
 
@@ -35,6 +42,15 @@ router.get('/machineConnectivity', dataController.getMachineConnectivity);
 router.get('/machines', dataController.getMachines);
 
 //getting single product by recipe id
-router.get('/getSingleProductbyRecipeID', spfdController.getSingleProductbyRecipeID)
+router.get(
+  '/getSingleProductbyRecipeID',
+  spfdController.getSingleProductbyRecipeID
+);
+
+//getting single recipe equipment usage
+router.get('/getSingleUsage/:date', eudController.getSingleUsage);
+
+// getting multiple recipe equipment usage
+router.get('/getMultipleUsage/:date', eudController.getMultipleUsage);
 
 module.exports = router;
---
+++ frontend/src/components/eud/UsageChart.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Chart } from 'react-google-charts';
+
+export const data = [
+  [
+    { type: 'string', id: 'Equipment Name' },
+    { type: 'string', id: 'Recipe Name' },
+    { type: 'date', id: 'Start' },
+    { type: 'date', id: 'End' },
+  ],
+];
+
+export const options = {
+  timeline: {
+    groupByRowLabel: true,
+  },
+};
+
+export function UsageChart(data) {
+  let data1 = data.data.data;
+  for (let i = 0; i < data1.length; i++) {
+    data1[i][2] = new Date(data1[i][2]);
+    data1[i][3] = new Date(data1[i][3]);
+  }
+
+  return (
+    <>
+      <Chart
+        chartType='Timeline'
+        data={data.data.data}
+        width='100%'
+        height='500px'
+        options={{
+          colors: [
+            '#9f8059',
+            '#2f4f4f',
+            '#556b2f',
+            '#8b4513',
+            '#6b8e23',
+            '#2e8b57',
+            '#7f0000',
+            '#191970',
+            '#006400',
+            '#708090',
+            '#bc8f8f',
+          ],
+        }}
+      />
+    </>
+  );
+}
