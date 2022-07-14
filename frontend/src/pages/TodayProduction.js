f34c982e340aea2d2ac153c26715f5b28aed42d9
George Yin Xu <georgeyinxu@gmail.com>
2022-07-14 22:10:20 +0800

    Fixed rebase conflict

--- frontend/src/pages/TodayProduction.js
+++ frontend/src/pages/TodayProduction.js
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import '../styles/styles.css';
-import ProductChart from '../components/tpd/ProductChart';
-import SideBar from '../components/sidebar/Sidebar';
+import React, { useEffect, useState } from "react";
+import "../styles/styles.css";
+import ProductChart from "../components/tpd/ProductChart";
+import TableComponent from "../components/tpd/TableComponent";
+import TextField from "@mui/material/TextField";
+import HorizontalScroller from 'react-horizontal-scroll-container';
+import { Tab } from "@mui/material";
 // import "../styles/todayProduction.css"
+
 const TodayProduction = () => {
   const [completedProductsData, setCompletedProductsData] = useState(null);
   const [toBeCompletedProductsData, setToBeCompletedProductsData] =
@@ -35,24 +39,50 @@ const TodayProduction = () => {
   }, []);
 
   return (
-    <div className='todayProduction row p-0 w-100p'>
-      <div className='tp-sidebar col-2'>
-        <SideBar />
-      </div>
-      <div className='tp-display col-10'>
-        <div className='App'>
-          {!isLoading ? (
-            <div className='Row1'>
-              <ProductChart
-                data1={completedProductsData}
-                data2={toBeCompletedProductsData}
-              />
+    <div className="App">
+      {!isLoading ? (
+        
+        <div>
+          <div className="tpdHeader">
+            Today's Production Dashboard
+          </div>
+          <div className="liveEquipHeader">
+            Live Equipment Usage
+          </div>
+          <div className="textField">
+                <TextField 
+                id="outlined-basic"
+                variant="outlined"
+                fullWidth
+                label="Search"
+                />
+          </div>
+          <div className="Row1">
+              <TableComponent />
+          </div>
+          <div className="productHeader">
+            Completed Products as of Today
+          </div>
+          <div className="textField">
+                <TextField 
+                id="outlined-basic"
+                variant="outlined"
+                fullWidth
+                label="Search"
+                />
+          </div>
+          <div className="Row2">
+            <div className="chartContainer">
+              <div class="p-3 bg-white-500 u-shadow-lg u-round-xs">
+                <ProductChart data1={completedProductsData} data2={toBeCompletedProductsData} />
+              </div>
             </div>
-          ) : (
-            <div> Loading... </div> // TODO: Loading loading spinner or loading component
-          )}
+          </div>
         </div>
-      </div>
+        
+      ) : (
+        <div> Loading... </div> // TODO: Loading loading spinner or loading component
+      )}
     </div>
   );
 };
