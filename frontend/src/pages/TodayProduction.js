b9542f162911c013b2937831b6bd54271778522f
George Yin Xu <georgeyinxu@gmail.com>
2022-07-14 22:14:03 +0800

    Fixed rebase conflict

--- frontend/src/pages/TodayProduction.js
+++ frontend/src/pages/TodayProduction.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
-import "../styles/styles.css";
-import ProductChart from "../components/tpd/ProductChart";
-import TableComponent from "../components/tpd/TableComponent";
-import TextField from "@mui/material/TextField";
+import React, { useEffect, useState } from 'react';
+import '../styles/styles.css';
+import ProductChart from '../components/tpd/ProductChart';
+import TableComponent from '../components/tpd/TableComponent';
+import TextField from '@mui/material/TextField';
 import HorizontalScroller from 'react-horizontal-scroll-container';
-import { Tab } from "@mui/material";
+import { Tab } from '@mui/material';
 // import "../styles/todayProduction.css"
 
 const TodayProduction = () => {
@@ -39,47 +39,42 @@ const TodayProduction = () => {
   }, []);
 
   return (
-    <div className="App">
+    <div className='App'>
       {!isLoading ? (
-        
         <div>
-          <div className="tpdHeader">
-            Today's Production Dashboard
+          <div className='tpdHeader'>Today's Production Dashboard</div>
+          <div className='liveEquipHeader'>Live Equipment Usage</div>
+          <div className='textField'>
+            <TextField
+              id='outlined-basic'
+              variant='outlined'
+              fullWidth
+              label='Search'
+            />
           </div>
-          <div className="liveEquipHeader">
-            Live Equipment Usage
+          <div className='Row1'>
+            <TableComponent />
           </div>
-          <div className="textField">
-                <TextField 
-                id="outlined-basic"
-                variant="outlined"
-                fullWidth
-                label="Search"
+          <div className='productHeader'>Completed Products as of Today</div>
+          <div className='textField'>
+            <TextField
+              id='outlined-basic'
+              variant='outlined'
+              fullWidth
+              label='Search'
+            />
+          </div>
+          <div className='Row2'>
+            <div className='chartContainer'>
+              <div class='p-3 bg-white-500 u-shadow-lg u-round-xs'>
+                <ProductChart
+                  data1={completedProductsData}
+                  data2={toBeCompletedProductsData}
                 />
-          </div>
-          <div className="Row1">
-              <TableComponent />
-          </div>
-          <div className="productHeader">
-            Completed Products as of Today
-          </div>
-          <div className="textField">
-                <TextField 
-                id="outlined-basic"
-                variant="outlined"
-                fullWidth
-                label="Search"
-                />
-          </div>
-          <div className="Row2">
-            <div className="chartContainer">
-              <div class="p-3 bg-white-500 u-shadow-lg u-round-xs">
-                <ProductChart data1={completedProductsData} data2={toBeCompletedProductsData} />
               </div>
             </div>
           </div>
         </div>
-        
       ) : (
         <div> Loading... </div> // TODO: Loading loading spinner or loading component
       )}
