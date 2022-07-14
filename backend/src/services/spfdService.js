b293c7669adc5dd490b9bd56f4926832b27c82b7
mememlol <88182648+mememlol@users.noreply.github.com>
2022-07-14 19:38:11 +0800

    Implemented UI for date and added backend for gantt chart

---
+++ backend/src/services/spfdService.js
@@ -0,0 +1,32 @@
+const Pool = require('../config/database');
+
+// still in progress
+
+module.exports.getSingleProductbyRecipeID = async function () {
+  try {
+    const { rows } = await Pool.query(
+      `WITH OMG as (
+        SELECT 
+          log_times.id,
+          log_times.equip_id, 
+          physical_devices.name as equip_name, 
+          log_times.recipe_id, 
+          recipes.name as recipe_name, 
+          log_times.log_action, 
+          LAG(log_times.log_time,1) OVER(ORDER BY log_times.id) start_time,
+          log_times.log_time as end_time, DATE(log_times.log_time) as date
+       FROM log_times
+       INNER JOIN physical_devices
+       ON log_times.equip_id = physical_devices.id
+      INNER JOIN recipes
+      ON log_times.recipe_id = recipes.id
+    )
+    SELECT id, equip_id, equip_name, recipe_id, recipe_name, start_time, end_time, -(start_time - end_time) as total_time, date 
+    FROM OMG 
+    WHERE log_action = 2 AND recipe_id = $1;`, [id]
+    ); // end of SQL query
+    return rows;
+  } catch (error) {
+    console.log(error);
+  }
+};
