f859e9557f7f66e14b5cb67a28f3666a4047a807
George Yin Xu <89299295+georgeyinxu@users.noreply.github.com>
2022-07-14 22:32:32 +0800

    Cb (#43)
    * Fixed rebase conflict #1
    
    * Fixed rebase conflict #2
    
    * Fixed rebase conflict #3
    
    * Fixed rebase conflict #4
    
    * Fixed rebase conflict #5
    
    * fixing routes to production Overview Dashboard
    
    * Fixed rebase conflict #6
    
    * popup shown according to data 1
    
    * changed the SQL query that is required for the Modal and fixed the radio buttons
    
    * testing git
    
    * fixing all the bugs in the recent pull
    
    * styling the modal component and retreiving data from backend
    
    * adding a form for react buttons
    
    * fixing redirect form for modal
    
    * restoring lost code
    
    * Fixed rebase conflict
    
    * Fixed rebase conflict
    
    * testing
    
    * testing2
    
    * Fixed rebase conflict
    
    * Fixed rebase conflict
    
    * Fixed rebase conflict
    
    * Fixed rebase conflict
    
    Co-authored-by: Adeeb <adityaadeeb04@gmail.com>
    Co-authored-by: deadgravy <86699462+deadgravy@users.noreply.github.com>
    Co-authored-by: Javier <JAVIERFOKJF11.21@ichat.sp.edu.sg>

--- backend/src/services/dataService.js
+++ backend/src/services/dataService.js
@@ -49,7 +49,7 @@ module.exports.getData1 = async function () {
 module.exports.getRecipebyRecipeID = async function (id) {
   try {
     const { rows } = await pool.query(
-      `SELECT 
+                        `SELECT 
                             rf.fr_recipe_id, r.name, rf.queue, t.fr_process_steps, ps.process_name, rf.desc_translate, AVG(t.duration)
                         FROM (SELECT  equip_id,
                             log_action,
@@ -233,17 +233,53 @@ module.exports.getMachines = async function () {
   }
 }
 
-module.exports.getMachines = async function () {
+module.exports.getEquipmentStatus = async function () {
   try {
-    const { rows } = await pool.query(`
-      SELECT
-        machine_name,
-        machine_id
-      FROM
-        log_station_clients
+    const { rows } =
+      await client.query(`SELECT pd.name, r.name AS productName, t.fr_process_steps, lt.log_time, pd.mac_property AS singleOrMultiple, lt.log_action, ('2021-08-21 08:00:00' - lt.log_time) AS timeToStarted, rf.queue, ps.process_name, rf.desc_translate, AVG(t.duration)
+      FROM ( SELECT  equip_id,
+      log_action,
+      fr_process_steps,
+      start_log_time,
+      end_log_time,
+      end_log_time - start_log_time AS duration
+      FROM    (   SELECT  equip_id,
+      log_action,
+      fr_process_steps,
+      log_time AS start_log_time,
+      (   SELECT  MIN(log_time)
+      FROM    (SELECT lt.equip_id, lt.fr_process_steps, lt.log_action, lt.log_time::timestamp FROM log_times lt
+      UNION ALL
+      SELECT null, mlt.work_type, mlt.action, mlt.log_time::timestamp FROM manual_log_times mlt) T2
+      WHERE   T2.fr_process_steps = T1.fr_process_steps
+      AND     T2.log_action = 2
+      AND     T2.log_time > T1.log_time
+      ) AS end_log_time
+      FROM    (     SELECT lt.equip_id, lt.fr_process_steps, lt.log_action, lt.log_time::timestamp FROM log_times lt
+      UNION ALL
+      SELECT null, mlt.work_type, mlt.action, mlt.log_time::timestamp FROM manual_log_times mlt) T1
+      WHERE T1.log_action = 1
+      ) AS T
+      ) AS t
+      INNER JOIN recipe_flows rf
+      ON rf.id = t.fr_process_steps
+      INNER JOIN recipes r
+      ON r.id = rf.fr_recipe_id
+      INNER JOIN process_steps ps
+      ON ps.id = rf.fr_process_step
+      INNER JOIN log_times lt
+      ON lt.recipe_id = rf.fr_recipe_id
+      INNER JOIN physical_devices pd
+      ON t.equip_id = pd.id
+      WHERE lt.log_time BETWEEN '2021-08-21 00:00:00' AND '2021-08-21 08:00:00'
+      AND pd.mac_property = 1
+      AND t.equip_id IS NOT NULL
+      GROUP BY pd.id, r.name, rf.queue, t.fr_process_steps, ps.process_name, rf.desc_translate, pd.name, lt.log_action, fr_recipe_id, lt.log_time, t.equip_id, t.log_action, pd.mac_property
+      ORDER BY t.equip_id, fr_recipe_id, lt.log_time;
       `);
+
     return rows;
   } catch (error) {
     console.log(error);
-  }
+  } 
 };
