19d6cf212da96328eeb36723ef3907471f0a66cd
George Yin Xu <georgeyinxu@gmail.com>
2022-07-19 20:33:54 +0800

    Added type of counter for the user to select counter

--- frontend/src/components/eus/EquipmentDetails.js
+++ frontend/src/components/eus/EquipmentDetails.js
@@ -4,24 +4,30 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Toggler from './Toggler';
+import CounterToggle from './CounterToggle';
 
 const EquipmentDetails = () => {
-    return (
-        <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls='panel1a-content'
-          id='panel1a-header'
-        >
-          <Typography className='text-xl font-bold pl-2'>
-            Equipment Details
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails className='row py-2'>
-            <Toggler />
-        </AccordionDetails>
-      </Accordion>
-    )
-}
+  return (
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls='panel1a-content'
+        id='panel1a-header'
+      >
+        <Typography className='text-xl font-bold pl-2'>
+          Equipment Frequency
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails className='row py-2'>
+        <div className='col-10'>
+          <Toggler />
+        </div>
+        <div className='col-2'>
+          <CounterToggle />
+        </div>
+      </AccordionDetails>
+    </Accordion>
+  );
+};
 
 export default EquipmentDetails;
