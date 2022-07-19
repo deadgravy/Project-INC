2073aeb3a42361c31994c75f0616c4dbbca79a0c
George Yin Xu <georgeyinxu@gmail.com>
2022-07-20 07:35:49 +0800

    Fixed rebase conflict #1

--- frontend/src/components/eus/Toggler.js
+++ frontend/src/components/eus/Toggler.js
@@ -1,8 +1,28 @@
+import { useState } from 'react';
 
 const Toggler = () => {
+  const [buttonState, setButtonState] = useState('toggle-button1');
+
+  function handleClick(e) {
+    if (buttonState !== e.target.id) {
+      setButtonState(e.target.id)
+    }
+    console.log(e.target.id)
+  }
+
   return (
-    <div>
-        
+    <div className='tri-state-toggle'>
+      <button className={buttonState == 'toggle-button1' ? 'tri-state-toggle-button active' : 'tri-state-toggle-button'} id='toggle-button1' onClick={handleClick}>
+        Daily
+      </button>
+
+      <button className={buttonState == 'toggle-button2' ? 'tri-state-toggle-button active' : 'tri-state-toggle-button'} id='toggle-button2' onClick={handleClick}>
+        Weekly
+      </button>
+
+      <button className={buttonState == 'toggle-button3' ? 'tri-state-toggle-button active' : 'tri-state-toggle-button'} id='toggle-button3' onClick={handleClick}>
+        Monthly
+      </button>
     </div>
   );
 };
