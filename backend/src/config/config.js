a1227701ace2706ef11c85dcfdf8ae3604a56093
Shelby <yeoxhshelby20.21@ichat.sp.edu.sg>
2022-07-21 20:52:16 +0800

    Configured env and gitignore files

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -3,6 +3,7 @@ dotenv.config(); //Build the process.env object.
 module.exports = {
   databaseUsername: process.env.DB_USERNAME,
   databasePassword: process.env.DB_PASSWORD,
+  databaseHost: process.env.DB_HOST,
   databaseName: process.env.DB_DATABASE_NAME,
   userDBName: process.env.DB_USERDB_NAME,
 
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,7 +3,7 @@ const config = require('./config');
 
 const db = new Pool({
   user: config.databaseUsername,
-  host: 'localhost',
+  host: config.databaseHost,
   password: config.databasePassword,
   database: config.databaseName,
   port: 5432,
