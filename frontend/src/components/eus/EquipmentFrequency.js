97f056723f4524ec21ab3992441d4f2409829cb2
George Yin Xu <89299295+georgeyinxu@users.noreply.github.com>
2022-07-23 22:11:34 +0800

    Feature equipment details (#52)
    * Set up of basic structure of equipment details section
    
    * Made the toggle button functional
    
    * Added type of counter for the user to select counter
    
    * Moved the toggler component into general
    
    * Set up of basic structure of equipment details section
    
    * Fixed rebase conflict #1
    
    * Added type of counter for the user to select counter
    
    * Moved the toggler component into general
    
    * Downloaded Victory Pie Charts into project and renamed my file to EquipmentFrequency
    
    * Fixed imports
    
    * Completed static pie chart for equipment frequency
    
    * Fixed rebase conflict #1
    
    * Fixed rebase conflict #1
    
    * Added type of counter for the user to select counter
    
    * Moved the toggler component into general
    
    * Fixed rebase conflict #1
    
    * Fixed rebase conflict #3
    
    * Moved the toggler component into general
    
    * Fixed rebase conflict #5
    
    * Completed static pie chart for equipment frequency

--- frontend/src/components/eus/EquipmentFrequency.js
+++ frontend/src/components/eus/EquipmentFrequency.js
@@ -5,8 +5,19 @@ import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Toggler from '../general/Toggler';
 import CounterToggle from './CounterToggle';
+import { VictoryPie } from 'victory';
 
 const EquipmentDetails = () => {
+  const equipmentFrequencyData = [
+    { x: 5, y: 10 },
+    { x: 14, y: 25 },
+    { x: 9, y: 20 },
+    { x: 11, y: 20 },
+    { x: 3, y: 5 },
+    { x: 10, y: 20 },
+  ];
+
+  const colorScheme = ['#39CEF3', 'orange', 'gold', '#0066FF', 'navy', '#800080'];
   return (
     <Accordion>
       <AccordionSummary
@@ -20,7 +31,23 @@ const EquipmentDetails = () => {
       </AccordionSummary>
       <AccordionDetails className='row py-2'>
         <div className='col-10'>
-          <Toggler />
+          <div className='toggler'>
+            <Toggler />
+          </div>
+          <div className='w-60p u-text-center'>
+            <h4 className='mt-2'>From 10th of August to 17th of August</h4>
+            <VictoryPie
+              colorScale={colorScheme}
+              data={equipmentFrequencyData}
+              innerRadius={100}
+              padAngle={5}
+              labelRadius={({ innerRadius }) => innerRadius + 15 }
+              cornerRadius={5}
+              style={{
+                labels: { fill: 'white', fontSize: 20, fontWeight: 'bold' },
+              }}
+            />
+          </div>
         </div>
         <div className='col-2'>
           <CounterToggle />
