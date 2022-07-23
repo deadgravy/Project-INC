647c9f33fb0d5afd8bdfc35ace702dcadae1b7a4
Shelby <yeoxhshelby20.21@ichat.sp.edu.sg>
2022-07-23 18:31:56 +0800

    Added frontend and backend for equipment usage details

--- backend/src/routes/dataRoutes.js
+++ backend/src/routes/dataRoutes.js
@@ -60,6 +60,17 @@ router.get(
 );
 
 router.get('/prodCount', dataController.prodCount);
+// getting single recipe equipment usage details
+router.get(
+  '/getSingleUsageDetails/:date/:hour',
+  eudController.getSingleUsageDetails
+);
+
+// getting multiple recipe equipment usage details
+router.get(
+  '/getMultipleUsageDetails/:date/:hour',
+  eudController.getMultipleUsageDetails
+);
 
 // getting single recipe equipment usage details
 router.get(
