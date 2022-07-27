79a4c2cb1f490ec18b376ef7f0660552e8e3515b
mememlol <88182648+mememlol@users.noreply.github.com>
2022-07-27 20:43:22 +0800

    fix merge conflics

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,15 +3,7 @@ const config = require('./config');
 
 const db = new Pool({
   user: config.databaseUsername,
-<<<<<<< HEAD
-<<<<<<< HEAD
-  host: config.databaseHost,
-=======
   host: 'localhost',
->>>>>>> 013d1181 (update for SPFD)
-=======
-  host: 'localhost',
->>>>>>> 013d1181 (update for SPFD)
   password: config.databasePassword,
   database: config.databaseName,
   port: config.portname,
