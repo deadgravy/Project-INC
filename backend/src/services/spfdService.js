58c366eece2c8bcbcfb894d398e7546eb395ddb7
mememlol <88182648+mememlol@users.noreply.github.com>
2022-07-30 16:54:00 +0800

    fix rebase conflic

--- backend/src/services/spfdService.js
+++ backend/src/services/spfdService.js
@@ -13,13 +13,12 @@ module.exports.getSingleProductWithNameDate = async function (name,startDate,end
           log_times.log_action, 
           LAG(log_times.log_time,1) OVER(ORDER BY log_times.id) start_time,
           log_times.log_time as end_time, DATE(log_times.log_time) as date
-<<<<<<< HEAD
         FROM log_times
         INNER JOIN physical_devices
         ON log_times.equip_id = physical_devices.id
         INNER JOIN recipes ON log_times.recipe_id = recipes.id                
       )
-      SELECT equip_name , recipe_name, start_time, end_time, -(start_time - end_time) as total_time, date
+      SELECT equip_name, recipe_name, start_time, end_time, -(start_time - end_time) as total_time, date
       FROM OMG 
       WHERE log_action = 2 AND recipe_name = $1 AND (date BETWEEN $2 and $3);`,
       [name,startDate,endDate]
@@ -35,27 +34,6 @@ module.exports.getAllRecipeName = async function () {
     const { rows } = await Pool.query(
       `SELECT name FROM public.recipes`
     );
-
-=======
-       FROM log_times
-       INNER JOIN physical_devices
-       ON log_times.equip_id = physical_devices.id
-      INNER JOIN recipes
-      ON log_times.recipe_id = recipes.id
-    )
-    SELECT equip_id, equip_name, recipe_id, recipe_name, start_time, end_time, -(start_time - end_time) as duration, date 
-<<<<<<< HEAD
-    FROM OMG
-=======
-    FROM OMG 
-<<<<<<< HEAD
->>>>>>> 13362ac9 (updated on UI of gantt chart)
-    WHERE log_action = 2 AND recipe_name = '$1' AND (date BETWEEN '$1' and '$1');`
-=======
-    WHERE log_action = 2 AND recipe_id = ?;`
->>>>>>> ac2a8846 (fix merge conflic)
-    ); // end of SQL query
->>>>>>> fe855c15 (resolve rebase conflics)
     return rows;
   } catch (error) {
     console.log(error);
