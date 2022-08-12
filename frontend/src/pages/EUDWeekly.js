5b1b3abfd9e10b84a7e4a4d28a0f34a9a88b6daf
Shelby <yeoxhshelby20.21@ichat.sp.edu.sg>
2022-08-12 17:47:41 +0800

    Changed component for multiple recipe equipment

--- frontend/src/pages/EUDWeekly.js
+++ frontend/src/pages/EUDWeekly.js
@@ -142,7 +142,7 @@ const EUDWeekly = () => {
                   multipleUsage.data.length === undefined ? (
                     <p>NO DATA</p>
                   ) : (
-                    <WeeklyChart data={multipleUsage} />
+                    <MREWeeklyChart data={multipleUsage} />
                   )}
                 </div>
                 <div className='row mt-4'>
