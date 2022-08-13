f82bf041cf36ac3af0f1e58a724fe0caa4e18f41
Shelby <yeoxhshelby20.21@ichat.sp.edu.sg>
2022-08-13 16:41:38 +0800

    Fixed rebase conflicts

--- frontend/src/pages/EquipUtilDashboard.js
+++ frontend/src/pages/EquipUtilDashboard.js
@@ -94,6 +94,11 @@ const EquipUtilDashboard = () => {
     setHours(hourinput);
   }
 
+  let singleData = {
+    data: singleUsage,
+    date: startDate,
+  };
+
   return (
     <React.StrictMode>
       <div className='equipmentuUtilisationDashboard row p-0 w-100p'>
