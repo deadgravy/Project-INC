e0bcf828288620e57f224f19682573db311fc7f7
George Yin Xu <georgeyinxu@gmail.com>
2022-08-14 16:01:23 +0800

    Added Swagger YAML and added documentation for some of the pages

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,14 @@ const dataRoutes = require('./src/routes/dataRoutes');
 const app = express();
 const cors = require('cors');
 app.use(cors());
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 //app.use(express.json());//Won't work to capture and reconstruct multipart/form-data
 //app.use(express.urlencoded({ extended: false }));//Won't work either
 app.use('/api/', dataRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const server = app.listen('4000', 'localhost', function () {
   let host = server.address().address;
