ed6b65e4580de340496a515f9883686cac3a0156
Adeeb <adityaadeeb04@gmail.com>
2022-08-15 00:30:07 +0800

    fixing rebase conflicts

--- backend/src/routes/dataRoutes.js
+++ backend/src/routes/dataRoutes.js
@@ -6,17 +6,10 @@ const dataController = require('../controllers/dataController');
 const loginController = require('../controllers/loginController');
 const userController = require('../controllers/userController');
 const spfdController = require('../controllers/spfdController');
+const eudController = require('../controllers/eudController');
 
 // get all recipes and ID
-<<<<<<< HEAD
-<<<<<<< HEAD
 router.get('/getAllRecipeAndID', dataController.getData1);
-=======
-router.get('/getAllRecipe', dataController.getData1);
->>>>>>> 8815b3dc (fixing data1 endpoint for api documentation)
-=======
-router.get('/getAllRecipeAndID', dataController.getData1);
->>>>>>> b3c7bd23 (fixing data2 endpoint for api documentation)
 
 // getting recipe by ID
 // router.get('/data/data2', dataController.getRecipebyRecipeID);
@@ -55,9 +48,6 @@ router.get(
   spfdController.getSingleProductWithNameDate
 );
 
-<<<<<<< HEAD
-router.get('/prodCount', dataController.prodCount);
-=======
 // getting single recipe equipment usage (eud)
 router.get('/getSingleUsage/:startdate/:enddate', eudController.getSingleUsage);
 
@@ -68,27 +58,56 @@ router.get(
 );
 
 router.get('/prodCount/:startDate/:endDate', dataController.prodCount);
->>>>>>> 9c502b1a (date picker not working)
+
+// getting single recipe equipment usage details (eud)
+router.get(
+  '/getSingleUsageDetails/:startdate/:enddate/:hour',
+  eudController.getSingleUsageDetails
+);
+
+// getting multiple recipe equipment usage details (eud)
+router.get(
+  '/getMultipleUsageDetails/:startdate/:enddate/:hour',
+  eudController.getMultipleUsageDetails
+);
 
 // getting the name of all recipes
 router.get('/getAllRecipeName', spfdController.getAllRecipeName);
 
-// Getting all equipments from log times table
-router.get('/getAllEquipments', dataController.getAllEquipments);
-
-// Getting start / stop count of specific equipment (eud)
-router.post(
-  '/getEquipmentStartOrStopCount',
-  dataController.getEquipmentStartOrStopCount
+// get weekly usage for single recipe equipment
+router.get(
+  '/getSingleUsageWeekly/:startdate/:enddate',
+  eudController.getSingleWeekly
 );
 
-router.post(
-  '/getAllEquipmentStartOrStop',
-  dataController.getAllEquipmentStartOrStopCount
+// get weekly usage for multiple recipe equipment
+router.get(
+  '/getMultipleUsageWeekly/:startdate/:enddate',
+  eudController.getMultipleWeekly
 );
 
-router.post('/getAnomolies', dataController.getAnomolies);
+// get weekly usage details  for single recipe equipment
+router.get(
+  '/getSingleUsageDetailsWeekly/:startdate/:enddate/:hour',
+  eudController.getSingleWeeklyDetails
+);
 
-router.post('/getAnomolies', dataController.getAnomolies);
+// get weekly usage details for multiple recipe equipment
+router.get(
+  '/getMultipleUsageDetailsWeekly/:startdate/:enddate/:hour',
+  eudController.getMultipleWeeklyDetails
+);
+
+// getting the equipment name for single product
+router.get(
+  '/getSingleProductEquipment/:startDate/:endDate/:name',
+  spfdController.getSingleProductEquipment
+);
+
+// getting the equipment usage by name for single product
+router.get(
+  '/getEquipmentUsageByName/:name/:ename',
+  spfdController.getEquipmentUsageByName
+);
 
 module.exports = router;
