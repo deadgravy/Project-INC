f476bd21967fd57ce2bfce489fb28f5da87d016a
George Yin Xu <georgeyinxu@gmail.com>
2022-08-16 16:55:06 +0800

    Fixed merge conflicts

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,7 +3,7 @@ const config = require('./config');
 
 const db = new Pool({
   user: config.databaseUsername,
-  host: 'localhost',
+  host: '10.211.55.6',
   password: config.databasePassword,
   database: config.databaseName,
   port: config.portname,
