f98737418ac2d0d2610a4a9abaec617f827a5114
mememlol <88182648+mememlol@users.noreply.github.com>
2022-08-17 17:03:46 +0800

    fix rebase conflics

--- backend/src/services/spfdService.js
+++ backend/src/services/spfdService.js
@@ -1,16 +1,16 @@
 const Pool = require('../config/database');
-
+ 
 module.exports.getSingleProductWithNameDate = async function (name,startDate,endDate) {
   try {
     const { rows } = await Pool.query(
       `WITH OMG as (
-        SELECT 
+        SELECT
           log_times.id,
-          log_times.equip_id, 
-          physical_devices.name as equip_name, 
-          log_times.recipe_id, 
-          recipes.name as recipe_name, 
-          log_times.log_action, 
+          log_times.equip_id,
+          physical_devices.name as equip_name,
+          log_times.recipe_id,
+          recipes.name as recipe_name,
+          log_times.log_action,
           LAG(log_times.log_time,1) OVER(ORDER BY log_times.id) start_time,
           log_times.log_time as end_time, DATE(log_times.log_time) as date
         FROM log_times
@@ -19,16 +19,16 @@ module.exports.getSingleProductWithNameDate = async function (name,startDate,end
         INNER JOIN recipes ON log_times.recipe_id = recipes.id                
       )
       SELECT equip_name, recipe_name, start_time, end_time
-      FROM OMG 
+      FROM OMG
       WHERE log_action = 2 AND recipe_name = $1 AND (date BETWEEN $2 and $3);`,
       [name,startDate,endDate]
-    ); // end of query; 
+    ); // end of query;
     return rows;
   } catch (error) {
     console.log(error);
   }
 };
-
+ 
 module.exports.getAllRecipeName = async function () {
   try {
     const { rows } = await Pool.query(
@@ -39,18 +39,20 @@ module.exports.getAllRecipeName = async function () {
     console.log(error);
   }
 };
-
+ 
+ 
+ 
 module.exports.getSingleProductEquipment = async function (name,startDate,endDate) {
   try {
     const { rows } = await Pool.query(
       `WITH OMG as (
-        SELECT 
+        SELECT
           log_times.id,
-          log_times.equip_id, 
-          physical_devices.name as equip_name, 
-          log_times.recipe_id, 
-          recipes.name as recipe_name, 
-          log_times.log_action, 
+          log_times.equip_id,
+          physical_devices.name as equip_name,
+          log_times.recipe_id,
+          recipes.name as recipe_name,
+          log_times.log_action,
           LAG(log_times.log_time,1) OVER(ORDER BY log_times.id) start_time,
           log_times.log_time as end_time, DATE(log_times.log_time) as date
         FROM log_times
@@ -59,25 +61,25 @@ module.exports.getSingleProductEquipment = async function (name,startDate,endDat
         INNER JOIN recipes ON log_times.recipe_id = recipes.id                
       )
       SELECT equip_name
-      FROM OMG 
+      FROM OMG
       WHERE log_action = 2 AND recipe_name = $1 AND (date BETWEEN $2 and $3);`,
       [name,startDate,endDate]
-    ); // end of query; 
+    ); // end of query;
     return rows;
   } catch (error) {
     console.log(error);
   }
 };
-
+ 
 module.exports.getEquipmentUsageByName = async function (name,ename) {
   try {
     const { rows } = await Pool.query(
       `WITH MFK as (
-        SELECT 
+        SELECT
          log_times.id,
-         log_times.equip_id, 
+         log_times.equip_id,
          physical_devices.name as equip_name,
-         recipes.name as recipe_name, 
+         recipes.name as recipe_name,
          log_times.log_action,
          LAG(log_times.log_time,1) OVER(ORDER BY log_times.id) start_time,
          log_times.log_time as end_time, DATE(log_times.log_time) as date
@@ -88,8 +90,8 @@ module.exports.getEquipmentUsageByName = async function (name,ename) {
       )
       SELECT date, ROUND(EXTRACT(EPOCH FROM -(start_time - end_time))/60) as total_time
       FROM MFK
-      WHERE log_action = 2 AND recipe_name = $1 AND equip_name = $2 AND (date BETWEEN $3 and $4);`,
-      [name,ename, startDate, endDate]
+      WHERE log_action = 2 AND recipe_name = $1 AND equip_name = $2`,
+      [name,ename]
     ); // end of query;
     return rows;
   } catch (error) {
