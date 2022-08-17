60cceb43f00b787c3510a7c6d70d12e7dbed369d
mememlol <88182648+mememlol@users.noreply.github.com>
2022-08-17 10:45:22 +0800

    fix rebase conflics

--- frontend/src/components/spfd/dropDown.js
+++ frontend/src/components/spfd/dropDown.js
@@ -1,37 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
-
-export default function DropDown({selectedProductFlow, setEquipmentNameFromDropdown}) {
+ 
+export default function DropDown({selectedProductFlow}) {
   const [dropDown, setDropDown] = useState([]);
   const formattedStartDate = selectedProductFlow.startDate.toLocaleDateString('zh-CN').substring(0,10).replaceAll("/", "-");
   const formattedEndDate = selectedProductFlow.endDate.toLocaleDateString('zh-CN').substring(0,10).replaceAll("/", "-");
-// const options = [
+  // const options = [
+ 
   // 'TWICE',
   // 'ITZY',
   // 'NMIXX'
   // ];
   // const options = dropDown
+ 
+ 
+ 
+ 
+ 
+ 
   useEffect(() =>{
     fetch(`http://localhost:4000/api/getSingleProductEquipment/${formattedStartDate}/${formattedEndDate}/${selectedProductFlow.recipeName}`)
       .then((res) => res.json())
       .then(data => {
         if(data.status === "success") {
           const formattedEquip = data.data.map((equipment) => equipment.equip_name)
-          // console.log(formattedEquip);
+          console.log(formattedEquip);
           setDropDown(Array.from(new Set([...formattedEquip])))
         }
     })
   },[])
-
+ 
   const onSelected = ({value, lable}) => {
-    // console.log(value);
+    console.log(value);
     setEquipmentNameFromDropdown(value)
-  };
-
-  return( 
-    
+  }
+  return(
+   
     <Dropdown onChange={onSelected} options={dropDown}  placeholder='Please select an equipment'/>
-
   );
 }
--- frontend/src/components/spfd/lineChart.js
+++ frontend/src/components/spfd/lineChart.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import { Chart } from "react-google-charts";
 import moment from 'moment';
@@ -52,14 +53,46 @@ export function LineChart({selectedProductFlow}) {
 
     <>
     {data && <Chart
+=======
+import React from "react";
+import { Chart } from "react-google-charts";
+
+export const data = [
+  [
+    "Date",
+    "Coconut Grater"
+  ],
+  ['2021-08-10', 34],
+  ['2021-08-13', 22],
+  ['2021-08-16', 14],
+  ['2021-08-17', 15],
+  ['2021-08-20', 31],
+  ['2021-08-21', 18]
+];
+
+export const options = {
+  chart: {
+    title: "Legend",
+    subtitle: "y-axis is the total time used in minutes (rounded up)",
+  },
+};
+
+export function LineChart() {
+  return (
+    <Chart
+>>>>>>> 693d3699 (added drop down and line chart)
       chartType="Line"
       width="100%"
       height="400px"
       data={data}
       options={options}
+<<<<<<< HEAD
     />}
     
     </>
     
+=======
+    />
+>>>>>>> 693d3699 (added drop down and line chart)
   );
 }
