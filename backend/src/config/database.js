cfa85af769a9e7e92db542d88f1e23b203408e54
mememlol <88182648+mememlol@users.noreply.github.com>
2022-08-17 09:46:16 +0800

    fix rebase conflics

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,7 +3,15 @@ const config = require('./config');
 
 const db = new Pool({
   user: config.databaseUsername,
+<<<<<<< HEAD
   host: 'localhost',
+=======
+<<<<<<< HEAD
+  host: config.databaseHost,
+=======
+  host: 'localhost',
+>>>>>>> 013d1181 (update for SPFD)
+>>>>>>> 398f92af (resolve rebase conflics)
   password: config.databasePassword,
   database: config.databaseName,
   port: config.portname,
