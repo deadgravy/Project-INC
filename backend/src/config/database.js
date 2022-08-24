692709deddf5db3c68b69fedd69efe8f8e538b9f
Shelby <yeoxhshelby20.21@ichat.sp.edu.sg>
2022-08-24 19:51:42 +0800

    Fixed rebase conflicts

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,7 +3,7 @@ const config = require('./config');
 
 const db = new Pool({
   user: config.databaseUsername,
-  host: 'aws-test2.cbmxncunwsgi.us-east-1.rds.amazonaws.com',
+  host: 'firc-teama.cbmxncunwsgi.us-east-1.rds.amazonaws.com',
   password: config.databasePassword,
   database: config.databaseName,
   port: config.portname,
