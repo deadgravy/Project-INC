84eff42568881b31bbebb3a591680939113c7032
Shelby <yeoxhshelby20.21@ichat.sp.edu.sg>
2022-08-28 23:19:35 +0800

    Added test case for TPD

--- backend/tests/tpdTest.js
+++ backend/tests/tpdTest.js
@@ -9,21 +9,51 @@ module.exports = {
       .setValue(emailTest, 'test@test.com')
       .setValue(pwTest, 'password')
       .click('.signInBtn')
-      .click("//*[@id='Today's Production']")
-      .waitForElementVisible('.App')
-      .waitForElementVisible('.react-datepicker-wrapper')
+      .waitForElementVisible('.EUS')
+      .click({
+        selector: '.row.level.px-1',
+        index: 2,
+      })
+      .expect.element('.todaysProd').to.be.visible;
+
+    // test for one date
+    browser
+      .click('.react-datepicker-wrapper')
+      .waitForElementVisible('.react-datepicker-popper')
+      .click('.react-datepicker-ignore-onclickoutside')
       .click('.react-datepicker__input-container')
-      .click('.react-datepicker__year-dropdown-container')
-      .click('.react-datepicker__year-option') // click 2021
-      .click('.react-datepicker__day--010');
+      .click(
+        '.react-datepicker__year-dropdown-container.react-datepicker__year-dropdown-container--scroll'
+      )
+      .click('.react-datepicker__year-option')
+      .click('.react-datepicker__day.react-datepicker__day--010');
 
-    //   .click('.singleProduct')
-    //   .waitForElementVisible('.modal-content')
-    //   .click('button#Vegetable Cutter')
+    // test for single product modal
+    browser.click('.single-btn');
+    browser.expect.element('.modal-content').to.be.visible;
+    browser.click({
+      selector: '.single-prod-btn',
+      index: 2,
+    });
 
-    //   .click('multipleProduct')
-    //   .waitForElementVisible('.modal-content')
-    //   .click('button#Big Steamer Box')
-    //   .waitForElementVisible('.table');
+    // test for multiple product modal
+    browser.click('.multi-btn');
+    browser.expect.element('.modal-content').to.be.visible;
+    browser.click({
+      selector: '.multi-prod-btn',
+      index: 2,
+    });
+
+    browser.expect.element('.chartSection').to.be.visible;
+
+    browser.click('.all-products'); //
+    browser.expect.element('.modal-content').to.be.visible;
+
+    browser.click({
+      selector: '.modal-btn',
+      index: 2,
+    });
+
+    browser.waitForElementVisible('.graph');
   },
 };
