e7fb0b209ee734ef00bb271dd0ce76ac879743b4
Shelby <yeoxhshelby20.21@ichat.sp.edu.sg>
2022-08-29 15:57:46 +0800

    Added test case for users page

--- frontend/src/pages/SingleProductFlow.js
+++ frontend/src/pages/SingleProductFlow.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import SideBar from '../components/sidebar/Sidebar';
 import Modal from '../components/spfd/modal';
-import '../styles/spfd.css';
+// import '../styles/spfd.css';
 import { GanttChart } from '../components/spfd/ganttChart';
 import DropDown from '../components/spfd/dropDown';
 import { LineChart } from '../components/spfd/lineChart';
